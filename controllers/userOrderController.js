0ed09f1013c62f6700c4e7202fa370255272d21b
ashkar2023 <muhammedashkar2023@gmail.com>
2024-02-05 18:24:31 +0530

    SSE header modified

--- controllers/userOrderController.js
+++ controllers/userOrderController.js
@@ -137,7 +137,7 @@ let sseClient = null;
 const sseSetup = (req,res)=>{
 	res.setHeader("Content-Type", "text/event-stream");
     res.setHeader("Cache-Control", "no-cache");
-    res.setHeader("Connection", "keep-alive");
+    res.setHeader("X-Accel-Buffering", "no");
 	
 	sseClient = res;
 
