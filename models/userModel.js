010449cd44e9446a57146cfb979525544a91f959
ashkar2023 <muhammedashkar2023@gmail.com>
2024-03-19 11:36:42 +0530

    Removed password select & fixed all footer

--- models/userModel.js
+++ models/userModel.js
@@ -12,8 +12,7 @@ const userSchema = new mongoose.Schema({
     },
     password:{
         type:String,
-        required:true,
-        select:false
+        required:true
     },
     phone:{
         type:Number,
